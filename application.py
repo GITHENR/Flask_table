0e21469cb7b6c7bb350674df0938b126c590325b
GITHENR <39644606+GITHENR@users.noreply.github.com>
2020-04-10 21:34:46 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World! This is from Python Flask Home Page"
+
+@app.route('/about')
+def about():
+    cntry = 'India'
+    return "<h2> Hello World from About page %s </h2>" %cntry
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("Profile.html",name=name)
