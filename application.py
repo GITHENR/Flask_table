fc72bbd1bb2b2ff6f234006547b6768c714a763e
GITHENR <39644606+GITHENR@users.noreply.github.com>
2020-04-14 16:50:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from azure.cosmosdb.table.tableservice import TableService
-table_service = TableService(account_name='cloudshell1200761026', account_key='e8CBA/8hhwpondb//pYIJuC0OlRU7VrzyRYbRNAm1BfPPI54e5oQ/iWzXlWEpiNW6hN4fvFmbNJ7ht0nPDXtiA==')
+table_service = TableService(account_name='cloudshell703046314', account_key='fTJlnFKqcwkuu4BCIJiOnIqGEB3aNBkY/yaZ55tm7UWKtzuTv5/pdHgzL2HunOGu8IuMHEEV92nMY0wi2ZANGw==')
 task = {'PartitionKey': 'first', 'RowKey': '002','ID': '0002', 'address': 'India','stock':40}
-table_service.insert_entity('customer1', task)
+table_service.update_entity('customer1', task)
 app = Flask(__name__)
 
 
